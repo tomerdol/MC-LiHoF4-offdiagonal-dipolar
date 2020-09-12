572d4d7eb72fbccb9659d6b0a9ef0b6cf933597c
Tomer <tomerdol@gmail.com>
2020-09-12 14:40:57 +0300

    refactored to IdeaProjects

---
+++ src/tests/simulation/montecarlo/SimulationCheckpointerTest.java
@@ -0,0 +1,23 @@
+package simulation.montecarlo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimulationCheckpointerTest {
+    SimulationCheckpointer checkpointer =
+            new SimulationCheckpointer("test", 4, 4, 0.0, true, 123456789);
+
+    @Before
+    public void setUp() throws Exception {
+        MonteCarloSimulation simulation=new MultipleTMonteCarloSimulation(new double[]{1.5,1.6,1.7}, null, 1, 1, null, false, false, false, true, checkpointer, 1,1,1);
+        checkpointer.writeCheckpoint((MultipleTMonteCarloSimulation) simulation);
+    }
+
+    @Test
+    public void readCheckpoint() {
+        MultipleTMonteCarloSimulation readSim = checkpointer.readCheckpoint();
+        assertEquals(5,readSim.tol,0.0001);
+    }
+}
