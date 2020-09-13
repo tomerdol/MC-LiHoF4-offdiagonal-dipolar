4efe793c48dd741b86c5a2b98dccdd0862337886
Tomer <tomerdol@gmail.com>
2020-09-13 17:03:28 +0300

    fixed line breaks in shell scripts

--- scripts/cont_sim.sh
+++ scripts/cont_sim.sh
@@ -24,22 +24,28 @@ COUNT=0
 for file in ./checkpoints/"$name"/save_state_"$L"_"$L"_"$H"_"$mech"_*.txt
 do
     seed=$(echo ${file##*_} | cut -f 1 -d '.')
-    
-    used_slots=`free_slot smoshe.q | grep sge1081 | cut -d' ' -f 2 | cut -d'/' -f 1`
-    #exclude sge1081 to leave at least 30 cores available for other users
-    queues="lublin.q,smoshe.q@sge1082,smoshe.q@sge190,fairshare.q"
-    
-    if [ "$used_slots" != "" ]; then
-    if [ $used_slots -le 24 ]; then
-    queues="lublin.q,smoshe.q,fairshare.q"
-    fi
-    fi
+    # check that the seed does not belong to an already running job
+    qstat -u tomerdol | awk 'NR>2 {print $1}' | xargs -n1 qstat -j | grep -q "$seed" &> /dev/null
+    if [ $? == 0 ]; then
+      used_slots=`free_slot smoshe.q | grep sge1081 | cut -d' ' -f 2 | cut -d'/' -f 1`
+      #exclude sge1081 to leave at least 30 cores available for other users
+      queues="lublin.q,smoshe.q@sge1082,smoshe.q@sge190,fairshare.q"
 
-    qsub -pe shared 24 -l mem_free=40G -V -S /bin/bash -cwd -N tr"$L"_"$H"_"$COUNT"_"$mech_initial" -o ./output/ -e ./output/ -q "$queues" ./scripts/met_with_t.sh "$L" "$L" "$max_sweeps" "$H" "$mech" "$name" "$seed"
-    #echo "${seeds[$i]}"
-    
-    ((COUNT++))
-    sleep 120
+      if [ "$used_slots" != "" ]; then
+      if [ $used_slots -le 24 ]; then
+      queues="lublin.q,smoshe.q,fairshare.q"
+      fi
+      fi
+
+      qsub -pe shared 24 -l mem_free=40G -V -S /bin/bash -cwd -N tr"$L"_"$H"_"$COUNT"_"$mech_initial" -o ./output/ -e ./output/ -q "$queues" ./scripts/met_with_t.sh "$L" "$L" "$max_sweeps" "$H" "$mech" "$name" "$seed"
+      #echo "${seeds[$i]}"
+
+      ((COUNT++))
+      sleep 120
+    else
+      jobid=$(qstat -u tomerdol | awk 'NR>2 {print $1}' | xargs -n1 qstat -j | grep -B26 ${seed} | awk 'NR==1 {print $2}')
+      echo "Seed ${seed} already running with job id ${jobid}."
+    fi
 done
 done
 done
