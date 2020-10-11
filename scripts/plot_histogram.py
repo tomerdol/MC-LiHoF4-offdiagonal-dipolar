44d119f6a077ab796a605096a6dae00fba2300d4
Tomer <tomerdol@gmail.com>
2020-10-11 17:32:32 +0300

    * Add smooth line to histograms * Add crystal+field+hamiltonian-transversal+field+const.py to create magnetic moment tables with constant Bz

--- scripts/plot_histogram.py
+++ scripts/plot_histogram.py
@@ -26,7 +26,9 @@ def parse_arguments():
 
 def main_hist2(simulations, to_plot, flip=False):
     from fit6 import str_with_err
+    from plot_bin import format_label
     fig, ax = plt.subplots(figsize=(10,10))
+    prop_iter = iter(plt.rcParams['axes.prop_cycle'])
     for to_plot_now in to_plot:
         num_of_simulations_to_plot=len(simulations)
         multiple_sim_data=[]
@@ -58,35 +60,56 @@ def main_hist2(simulations, to_plot, flip=False):
             shared_bins_to_plot = shared_bins[:-1] + i*np.diff(shared_bins)/num_of_simulations_to_plot
 
             histograms = np.zeros((num_independent_runs, len(shared_bins)-1))
+            smooth_histogram_x = np.linspace(shared_bins[0],shared_bins[-1],num=200)
+            smooth_histogram = np.zeros((num_independent_runs, len(smooth_histogram_x)))
             simple_values=np.zeros(num_independent_runs)
             abs_values=np.zeros(num_independent_runs)
             standard_deviations=np.zeros(num_independent_runs)
+            window_size=5
             for i in range(num_independent_runs):
                 simple_values[i] = np.mean(data[groups==i])
                 abs_values[i] = np.mean(np.abs(data[groups==i]))
                 standard_deviations[i] = np.sqrt(np.mean(data[groups==i]**2))
                 histograms[i], _ = np.histogram(data[groups == i], bins=shared_bins)
+
+                for idx, x in enumerate(smooth_histogram_x):
+                    bin_width = shared_bins[1]-shared_bins[0]
+                    low_bound = x - bin_width*(window_size*0.5)
+                    upper_bound = x + bin_width*(window_size*0.5)
+                    smooth_histogram[i,idx] = np.sum((groups==i) & (data > low_bound) & (data<upper_bound))/(window_size)
             simple_values_mean = simple_values.mean()
             simple_values_err = simple_values.std()/np.sqrt(simple_values.size-1)
             abs_values_mean = abs_values.mean()
             abs_values_err = abs_values.std()/np.sqrt(abs_values.size-1)
             sd_mean = standard_deviations.mean()
             sd_err = standard_deviations.std()/np.sqrt(standard_deviations.size-1)
-            print(list(sim))
-            print("-"*len(str(list(sim))))
+
+            print(format_label(list(sim)[2:],format=['L','folderName','Bex','mech','T']))
+            print("-"*len(format_label(list(sim)[2:],format=['L','folderName','Bex','mech','T'])))
             print("MEAN: " + str(simple_values_mean) + ' +- ' + str(simple_values_err))
             print("ABS: " + str(abs_values_mean) + ' +- ' + str(abs_values_err))
             print("SD: " + str(sd_mean) + ' +- ' + str(sd_err))
-            plt.bar(shared_bins_to_plot, np.mean(histograms,axis=0), align='edge', width=np.diff(shared_bins)/num_of_simulations_to_plot, label="%s, MEAN=%s, ABS=%s, SD=%s" % (list(sim), str_with_err(simple_values_mean,simple_values_err), str_with_err(abs_values_mean,abs_values_err), str_with_err(sd_mean,sd_err)),
-                    yerr=np.std(histograms,axis=0)/np.sqrt(num_independent_runs-1))
+            plt.bar(shared_bins_to_plot, np.mean(histograms,axis=0), alpha=0.9, align='edge', width=np.diff(shared_bins)/num_of_simulations_to_plot, label="%s, MEAN=%s, ABS=%s, SD=%s" % (format_label(list(sim)[2:],format=['L','folderName','Bex','mech','T']), str_with_err(simple_values_mean,simple_values_err), str_with_err(abs_values_mean,abs_values_err), str_with_err(sd_mean,sd_err)),
+                    yerr=np.std(histograms,axis=0)/np.sqrt(num_independent_runs-1), color=next(prop_iter)['color'])
+
+
+            plt.plot(smooth_histogram_x, np.mean(smooth_histogram,axis=0),label="Smooth %s 2"%format_label(list(sim)[2:],format=['L','folderName','Bex','mech','T']), color=next(prop_iter)['color'])
 
             if flip:
                 data=multiple_sim_data[sim_index+1]
                 groups=multiple_sim_groups[sim_index+1]
+                smooth_histogram_x = np.linspace(shared_bins[0],shared_bins[-1],num=200)
+                smooth_histogram = np.zeros((num_independent_runs, len(smooth_histogram_x)))
                 for i in range(num_independent_runs):
                     histograms[i], _ = np.histogram(data[groups == i], bins=shared_bins)
-                plt.bar(shared_bins_to_plot, np.mean(histograms,axis=0), align='edge', width=np.diff(shared_bins)/num_of_simulations_to_plot, label='Flipped %s' % list(sim), alpha=0.5,
-                        yerr=np.std(histograms,axis=0)/np.sqrt(num_independent_runs-1), ecolor='b')
+                    for idx, x in enumerate(smooth_histogram_x):
+                        bin_width = shared_bins[1]-shared_bins[0]
+                        low_bound = x - bin_width*(window_size*0.5)
+                        upper_bound = x + bin_width*(window_size*0.5)
+                        smooth_histogram[i,idx] = np.sum((groups==i) & (data > low_bound) & (data<upper_bound))/(window_size)
+                plt.bar(shared_bins_to_plot, np.mean(histograms,axis=0), align='edge', width=np.diff(shared_bins)/num_of_simulations_to_plot, label='Flipped %s' % format_label(list(sim)[2:],format=['L','folderName','Bex','mech','T']), alpha=0.5,
+                        yerr=np.std(histograms,axis=0)/np.sqrt(num_independent_runs-1), ecolor='b', color=next(prop_iter)['color'])
+                plt.plot(smooth_histogram_x, np.mean(smooth_histogram,axis=0),label="Flipped smooth %s"%format_label(list(sim)[2:],format=['L','folderName','Bex','mech','T']), color=next(prop_iter)['color'])
 
     plt.legend()
     plt.grid()
