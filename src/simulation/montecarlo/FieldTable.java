719443d4b7d61d010c93c94bdff3dab2daeb22d3
Tomer <tomerdol@gmail.com>
2020-12-29 14:23:05 +0200

    CRUCIAL FIX: call to manualCalcValue always called for energy and never for magnetic moment!

--- src/simulation/montecarlo/FieldTable.java
+++ src/simulation/montecarlo/FieldTable.java
@@ -523,19 +523,14 @@ final public class FieldTable {
         double ret;
         boolean status=true;
         try{
-            if (Math.abs(bz)>0.029) {
-                ret = findInTable(bx, by, bz, prevBIndices);
-            }else{
-                ret = findInTable(bx, by, bz, prevBIndices);
-//                ret = manualCalcValue(bx, by, bz, this.name.substring(0, name.length() - 9).equals("magnetic_moment_up_broyden"));
-            }
+            ret = findInTable(bx, by, bz, prevBIndices);
         } catch(IndexOutOfBoundsException e){
             //bx, by or bz are outside the range of the table
             // so the the value calculated directly
             // this takes a lot of time and is not recommended as a frequent solution
 
             // the call and name "magnetic_moment_up_broyden" is kept to preserve compatibility with the python script
-            ret = manualCalcValue(bx, by, bz, this.name.substring(0, name.length() - 9).equals("magnetic_moment_up_broyden"));
+            ret = manualCalcValue(bx, by, bz, this.name.startsWith("magnetic_moment"));
             status=false;
             //System.err.println("field was not in table. " + name.substring(0, name.length() - 20) + " had to be calculated manually using python. Bx=" + bx + " By=" + by + " Bz=" + bz + "|| the value calculated is: " + ret);
         }
